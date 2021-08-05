70290e42232b302f0f5465034ad4e7d04b450faf
Priya <prakashlakshmipriya@gmail.com>
2021-08-05 09:34:32 -0700

    coded getUser(), follow(), getFollows(), and getFollowers()

--- social.js
+++ social.js
@@ -9,32 +9,53 @@ class SocialNetwork {
 
   addUser(name) {
     // Fill this out
+    //'1' : {'id': 1, 'name': 'John Doe'}
+    this.currentID++
 
+    let user = {};
+    user.id = this.currentID;
+    user.name = name;
+
+    let id = this.currentID
+    this.users[id] = user;
+    this.follows[id] = new Set();
+
+    return this.currentID;
 
   }
 
   getUser(userID) {
+    if (this.users[userID] === undefined) return null;
 
-    // Fill this out
-
+    return this.users[userID];
   }
 
   follow(userID1, userID2) {
+    if(this.users[userID2] === undefined) return false
 
-    // Fill this out
+    this.follows[userID1].add(userID2);
+    return true;
 
   }
 
   getFollows(userID) {
 
-    // Fill this out
+    return this.follows[userID];
 
   }
 
   getFollowers(userID) {
+    //this.follows => { '1': Set { 2 }, '2': Set {} }
+    //need to check if any other user follows the given userID
 
-    // Fill this out
+    let followers = new Set();
+    for (let key in this.follows) {
+      if(this.follows[key].has(userID)) {
+        followers.add(Number(key))
+      }
+    }
 
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
